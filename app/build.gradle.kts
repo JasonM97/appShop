f8be9e5ae5d11dc7ebf710ceddff2254ffc15d03
JasonM97 <jason97gts@gmail.com>
2025-11-22 18:08:37 -0500

    Desarrollo de interffaz de Login y Menu Principal

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.apptienda"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.apptienda"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.annotation)
+    implementation(libs.gridlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //Dependencia pa poner imagens de Lottie
+    implementation("com.airbnb.android:lottie:6.4.0")
+    implementation ("com.google.android.material:material:1.10.0")
+
+
+}
---
+++ app/src/main/java/com/example/apptienda/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.apptienda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.apptienda.databinding.LoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginBinding binding;
+
+
+    //DECLARACION DE LAS VARAIBLES PARA AUTENTIFICACION
+    private EditText usuario;
+    private EditText contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide(); // OCULTA LA BARRA SUPERIOR
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        //ENLAZAMIENTO CON LA INTERFAZ Login - lOGINaCTIVITY
+
+            //Se guarda lo que esta en la caja de la interfaz , en la variable declarada de manera privada
+            usuario = findViewById(R.id.txtUsuario);
+            contraseña = findViewById(R.id.txtPassword);
+
+        //DIRECCIONAR A AL MENU DEL NAVIGATION BUTTON (Main Activity )
+
+            //conecta el botón del layout con el código, para poder controlarlo.
+            Button btnIniciar = findViewById(R.id.btnLogin);
+            btnIniciar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String usuarioInput = usuario.getText().toString().trim();
+                    String contraseñaInput = contraseña.getText().toString().trim();
+
+                    // Validación por defecto
+                    if(usuarioInput.equals("admin") && contraseñaInput.equals("1234")) {
+                        Toast.makeText(LoginActivity.this, "¡Login correcto!", Toast.LENGTH_SHORT).show();
+
+                        // Aquí nos dirige a la interfaz MainActivity
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+
+    }
+}
